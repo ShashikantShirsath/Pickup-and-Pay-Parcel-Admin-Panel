2a05760d75bfb64c2700650fd66501a32c235d99
Vaishnavi Vikhe <vaishnavi@bitviraj.com>
2025-01-23 08:28:28 +0530

    Make changes

---
+++ src/MyComponents/ParcelManagement/ParcelData.js
@@ -0,0 +1,11 @@
+const parcelData = [
+    { id: 1, name: "Parcel A", status: "Pickup" },
+    { id: 2, name: "Parcel B", status: "In Transit" },
+    { id: 3, name: "Parcel C", status: "Delivered" },
+    { id: 4, name: "Parcel D", status: "Pickup" },
+    { id: 5, name: "Parcel E", status: "In Transit" },
+    { id: 6, name: "Parcel F", status: "Delivered" },
+  ];
+  
+  export default parcelData;
+  
